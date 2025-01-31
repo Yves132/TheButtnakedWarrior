e2b09f7e24a427ec655a3903c28c74fa30dfad5e
Yves132 <solo.yves.art@gmail.com>
2025-01-31 21:21:45 +0100

    update 31/01/2025-2
    Made better healthbars and fixed visual bugs on some labels.

--- Scripts/Enemies/HealthBar.gd
+++ Scripts/Enemies/HealthBar.gd
@@ -1,6 +1,12 @@
 extends Control
 
+@onready var damage_bar = $ProgressBar/ProgressBar
+@onready var health_bar = $ProgressBar
 	
 func update_health(max_health, health):
-	$ProgressBar.max_value = max_health
-	$ProgressBar.value = health
+	health_bar.max_value = max_health
+	damage_bar.max_value = max_health
+	health_bar.value = health
+	await get_tree().create_timer(0.4).timeout
+	damage_bar.value = health
+	
